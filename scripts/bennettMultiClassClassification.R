acc06b2ab4e1180c0042b01ff682f6202985487c
bennettbishop1 <bennettbishop@ucsb.edu>
2024-11-19 13:56:19 -0800

    Save results in csv file

--- scripts/bennettMultiClassClassification.R
+++ scripts/bennettMultiClassClassification.R
@@ -15,6 +15,7 @@ library(keras)
 # load cleaned data
 load("data/bennett-claims-clean-example.RData")
 
+
 # partition
 set.seed(110122)
 partitions <- claims_clean %>%
@@ -24,6 +25,14 @@ train_text <- training(partitions) %>%
   pull(text_clean)
 train_labels <- training(partitions) %>%
   pull(mclass)
+
+# Test text for later
+test_text <- testing(partitions) %>%
+  pull(text_clean)
+test_labels <- testing(partitions) %>%
+  pull(mclass)
+test_ids <- testing(partitions) %>%
+  pull(.id)
 
 # create a preprocessing layer
 preprocess_layer <- layer_text_vectorization(
@@ -51,3 +60,21 @@ svm_model <- svm(
 
 # Save Model
 saveRDS(svm_model, file = "results/bennett-multiclass-model.rds")
+
+# Load test data
+load("data/claims-test.RData")
+
+# Preprocess test data using the preprocessing layer
+test_tfidf <- as.array(preprocess_layer(test_text))
+
+# Make predictions on the test data
+test_predictions <- predict(svm_model, newdata = test_tfidf)
+
+# Create the predictions tibble
+pred_df <- tibble(
+  .id = test_ids,
+  mclass.pred = test_predictions
+)
+
+# Save predictions as a CSV file
+write_csv(pred_df, "results/bennett-multiclass-predictions.csv")
